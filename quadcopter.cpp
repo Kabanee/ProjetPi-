d5a45b76ea11dceb621a0d5d47d122568e8e5e19
Louis-Guillaume <logino2503@gmail.com>
2021-11-21 16:45:11 +0100

    Commentaire <windows.h>

--- quadcopter.cpp
+++ quadcopter.cpp
@@ -15,7 +15,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
-#include <windows.h>
+//#include <windows.h>
 #include <thread>
 #include <mutex>
 #include <stdlib.h>
